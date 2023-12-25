361e676d2e7f78b4bf76e89831bf3a9f27145958
Dhruv786Sharma <115816524+Dhruv786Sharma@users.noreply.github.com>
2023-12-25 13:10:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function preLoad()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(400, 400);
+    canvas.position(500, 250);
+   
+}
+
+function draw()
+{
+    rect(10, 10, 380, 380, 80);
+    circle(100, 60, 55);
+    circle(300, 60, 55);
+    rect(170, 100, 55, 150, 20);
+    ellipse(195, 320, 150, 65);
+    strokeWeight(10);
+    stroke(51);
+
+
+
+}
+
+function take_snapshot()
+{
+    
+}
